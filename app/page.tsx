c2b4a7a06bf052f716803b0b8f1504c53b809189
David Munro <munrodj86@gmail.com>
2023-07-22 19:22:37 -0400

    fix: use async components

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,8 @@
 import { getAllRounds } from '@/lib/getAllRounds'
 import Image from 'next/image'
 
-export async function getServerSideProps() {
+export default async function Home() {
   const rounds = await getAllRounds()
-
-  // Return the rounds as a prop
-  return { props: { rounds } }
-}
-
-export default function Home({ rounds }: { rounds: Round[]}) {
 
   return (
     <pre>{JSON.stringify(rounds, null, 2)}</pre>
--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from 'drizzle-kit';
 import dotenv from 'dotenv';
 
-dotenv.config({ path: '.env' });
+dotenv.config({ path: '.env.local' });
 
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is not defined");
