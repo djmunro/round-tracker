90e9b4ac879e6276e55fc7e37f22ba8491e8ddfb
David Munro <munrodj86@gmail.com>
2023-07-22 18:39:37 -0400

    feat: Update home page to fetch and display all rounds

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
+import { getAllRounds } from '@/lib/getAllRounds'
 import Image from 'next/image'
 
-export default function Home() {
+export default async function Home() {
+  const rounds = await getAllRounds()
+
+  return (
+    <pre>{JSON.stringify(rounds, null, 2)}</pre>
+  )
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
